d3ebc6cb43d598dfb7b4d43d81204e48910177e5
Roberto Rivera S <rriveras@gmail.com>
2012-01-26 11:09:19 -0300

    Improved messages

--- bash/git_info.sh
+++ bash/git_info.sh
@@ -8,31 +8,31 @@
 # Based on discussion at http://kerneltrap.org/mailarchive/git/2007/11/12/406496
 
 pushd . >/dev/null
-ECYAN='\e[1;36m'
+
 # Find base of git directory
 while [ ! -d .git ] && [ ! `pwd` = "/" ]; do cd ..; done
 
 # Show various information about this git directory
 if [ -d .git ]; then
-  echo -e "\033[0;36m############ Remote URL: ####################\033[0m"
+  echo -e "\033[0;36m~~~~~~~~~~~~ Remote URL: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m"
   git remote -v
   echo
 
-  echo -e "\033[0;36m############ Remote Branches: ###############\033[0m"
+  echo -e "\033[0;36m~~~~~~~~~~~~ Remote Branches: ~~~~~~~~~~~~~~~~~~~~~~~\033[0m"
   git branch -r
   echo
 
-  echo -e "\033[0;36m############ Local Branches: ################\033[0m"
+  echo -e "\033[0;36m~~~~~~~~~~~~ Local Branches: ~~~~~~~~~~~~~~~~~~~~~~~~\033[0m"
   git branch
   echo
 
-  echo -e "\033[0;36m############ Latest Commit ##################\033[0m"
+  echo -e "\033[0;36m~~~~~~~~~~~~ Latest Commit: ~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m"
   git log --max-count=1
   echo
 
-  echo -e "\n=> Type 'git log' for more commits, or 'git show' for full commit details.\n"
+  echo -e "\033[0;32m=> Type 'git log' for more commits, or 'git show' for full commit details.\n"
 else
-  echo -e "\n=> Not a git repository.\n"
+  echo -e "\033[0;31m=> Not a git repository.\n"
 fi
 
 popd >/dev/null
