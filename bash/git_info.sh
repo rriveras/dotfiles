052476ddc8a5b29f9e37b31282c90ddf81071b97
Roberto Rivera S <rriveras@gmail.com>
2012-01-26 00:16:43 -0300

    More readability to git_info script

--- bash/git_info.sh
+++ bash/git_info.sh
@@ -8,33 +8,31 @@
 # Based on discussion at http://kerneltrap.org/mailarchive/git/2007/11/12/406496
 
 pushd . >/dev/null
-
+ECYAN='\e[1;36m'
 # Find base of git directory
 while [ ! -d .git ] && [ ! `pwd` = "/" ]; do cd ..; done
 
 # Show various information about this git directory
 if [ -d .git ]; then
-  echo "== Remote URL: `git remote -v`"
+  echo -e "\033[0;36m############## Remote URL: ###############\033[0m\n"
+  git remote -v
+  echo
 
-  echo "== Remote Branches: "
+  echo -e "\033[0;36m############ Remote Branches: ############\033[0m\n"
   git branch -r
   echo
 
-  echo "== Local Branches:"
+  echo -e "\033[0;36m############# Local Branches: #############\033[0m\n"
   git branch
   echo
 
-  echo "== Configuration (.git/config)"
-  cat .git/config
-  echo
-
-  echo "== Most Recent Commit"
+  echo -e "\033[0;36m############## Latest Commit ##############\033[0m\n"
   git log --max-count=1
   echo
 
-  echo "Type 'git log' for more commits, or 'git show' for full commit details."
+  echo -e "\n=> Type 'git log' for more commits, or 'git show' for full commit details.\n"
 else
-  echo "Not a git repository."
+  echo -e "\n=> Not a git repository.\n"
 fi
 
 popd >/dev/null
